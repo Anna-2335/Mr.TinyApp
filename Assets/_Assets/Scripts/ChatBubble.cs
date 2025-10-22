4c1170d09317e8479566576e413a0d73dfadebc4
Anna-2335 <annamaruyama1@gmail.com>
2025-10-21 19:02:49 -0700

    Press E ui edit

--- Assets/_Assets/Scripts/ChatBubble.cs
+++ Assets/_Assets/Scripts/ChatBubble.cs
@@ -8,12 +8,12 @@ public class ChatBubble : MonoBehaviour
     private SpriteRenderer backgroundSpriteRenderer;
     private SpriteRenderer iconSpriteRenderer;
     private TextMeshPro textMeshPro;
-    
-  
-    
+
+
+
     private void Awake()
     {
-        backgroundSpriteRenderer= transform.Find("Background").GetComponent<SpriteRenderer>();
+        backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
         iconSpriteRenderer = transform.Find("Icon").GetComponent<SpriteRenderer>();
         textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
 
@@ -23,13 +23,26 @@ public class ChatBubble : MonoBehaviour
     {
         Setup("Welcome to the Tiny Library!");
     }
-    private void Setup(string text)
+    public void Setup(string text)
     {
-        textMeshPro.SetText (text);
+        textMeshPro.SetText(text);
         textMeshPro.ForceMeshUpdate();
-        Vector2 textSize= textMeshPro.GetRenderedValues(false);
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
 
-        Vector2 padding = new Vector2 (10f, 2f);
+        Vector2 padding = new Vector2(10f, 2f);
         backgroundSpriteRenderer.size = textSize + padding;
+        StartCoroutine(HideAfterSeconds(4f));
+
+    }
+
+    private IEnumerator HideAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        // Hide this chat bubble
+        gameObject.SetActive(false);
+        // Optionally, trigger the next chat bubble
+        // For example, if you have a ChatManager:
+        // ChatManager.Instance.ShowNextBubble();
     }
 }
--- Assets/_Assets/Scripts/NPCInteractable.cs
+++ Assets/_Assets/Scripts/NPCInteractable.cs
@@ -4,7 +4,7 @@ public class NPCInteractable : MonoBehaviour
 {
     public void Interact()
     {
-        Debug.Log("Interact");
+        //ChatBubble.Create(transform, new Vector3(-3f, 1.7f, 0f), ChatBubble.Text, "Hewlllo");
     }
 
 }
